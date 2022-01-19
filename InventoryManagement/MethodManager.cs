ba1618a5d08c3af7766c1e3500bdb2ccff4d03d2
Lenovo <somusp75@gmail.com>
2022-01-19 22:59:23 +0530

    Add project files.

---
+++ InventoryManagement/MethodManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static InventoryManagement.InventorySupport;
+
+namespace InventoryManagement
+{
+    internal interface  MethodManager
+    {
+        //Display methods
+        void DisplayInventory(List<Rice> ricelist);
+        void DisplayInventory(List<Wheat> wheatList);
+        void DisplayInventory(List<Pulse> pulseList);
+        //Add Methods
+        void AddInventory(List<Wheat> wheatList);
+        void AddInventory(List<Pulse> pulseList);
+        void AddInventory(List<Rice> ricelist);
+        //Delete Methods
+        void DeleteInventory(List<Rice> riceList);
+        void DeleteInventory(List<Pulse> pulseList);
+        void DeleteInventory(List<Wheat> wheatList);
+    }
+}
